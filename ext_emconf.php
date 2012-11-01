251767a88077daa744e50d49e1ded191a6e27121
vbmazter <vbmazter@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-01 15:07:51 +0000

    upload to TER -> 0.1.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mb_olc_flights/trunk@67576 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mb_olc_flights".
 #
-# Auto generated 26-10-2012 10:11
+# Auto generated 01-11-2012 15:05
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,22 +14,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'mb_olc_flights',
 	'description' => 'Displays OLC flights (soaring) by date, shows best flight and allows for links to aircraft details.',
 	'category' => 'fe',
-	'author' => 'Martin Becker',
-	'author_email' => 'vbmazter@web.de',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '0.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => 'cm1',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Martin Becker',
+	'author_email' => 'vbmazter@web.de',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"552d";s:29:"class.tx_mbolcflights_cm1.php";s:4:"32e3";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"9e05";s:14:"ext_tables.php";s:4:"4066";s:13:"locallang.xml";s:4:"18ca";s:16:"locallang_db.xml";s:4:"f4dc";s:10:"README.txt";s:4:"ee2d";s:13:"cm1/clear.gif";s:4:"cc11";s:15:"cm1/cm_icon.gif";s:4:"8074";s:12:"cm1/conf.php";s:4:"d2ba";s:13:"cm1/index.php";s:4:"dcd3";s:17:"cm1/locallang.xml";s:4:"885a";s:19:"doc/wizard_form.dat";s:4:"fb08";s:20:"doc/wizard_form.html";s:4:"36cf";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:33:"pi1/class.tx_mbolcflights_pi1.php";s:4:"f316";s:41:"pi1/class.tx_mbolcflights_pi1_wizicon.php";s:4:"8848";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"53ed";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"65ab";s:29:"class.tx_mbolcflights_cm1.php";s:4:"32e3";s:12:"ext_icon.gif";s:4:"bd34";s:17:"ext_localconf.php";s:4:"0e22";s:14:"ext_tables.php";s:4:"3812";s:19:"flexform_ds_pi1.xml";s:4:"9d9d";s:13:"locallang.xml";s:4:"18ca";s:16:"locallang_db.xml";s:4:"29ce";s:10:"README.txt";s:4:"8818";s:13:"cm1/clear.gif";s:4:"cc11";s:15:"cm1/cm_icon.gif";s:4:"bd34";s:12:"cm1/conf.php";s:4:"d2ba";s:13:"cm1/index.php";s:4:"dcd3";s:17:"cm1/locallang.xml";s:4:"885a";s:21:"doc/example_setup.txt";s:4:"fb71";s:14:"doc/manual.pdf";s:4:"8fd3";s:14:"doc/manual.sxw";s:4:"2cae";s:19:"doc/wizard_form.dat";s:4:"fb08";s:20:"doc/wizard_form.html";s:4:"36cf";s:14:"pi1/ce_wiz.gif";s:4:"bd34";s:33:"pi1/class.tx_mbolcflights_pi1.php";s:4:"53d1";s:41:"pi1/class.tx_mbolcflights_pi1_wizicon.php";s:4:"8848";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"b916";s:22:"pi1/olc_reader.php.inc";s:4:"de1f";s:27:"pi1/simple_html_dom.php.inc";s:4:"1f38";s:16:"res/template.css";s:4:"f320";s:17:"res/template.html";s:4:"a15d";s:24:"res/template_simple.html";s:4:"d411";s:20:"static/css/setup.txt";s:4:"1ea7";}',
+	'suggests' => array(
+	),
 );
 
 ?>
